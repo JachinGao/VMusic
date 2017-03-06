ac42366eb93466c78e7d0ec5daa64665002d8f17
JachinGao <winter_2013@163.com>
2017-03-06 22:52:23 +0800

    Initial commit

---
+++ app/src/main/java/com/jachin/vmusic/fragments/MusicFragment.java
@@ -0,0 +1,25 @@
+package com.jachin.vmusic.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.jachin.vmusic.R;
+
+/**
+ * Created by Gaozhaochen on 2017/3/6.
+ */
+
+public class MusicFragment extends BaseFragment {
+    public static MusicFragment newInstance(){
+        return new MusicFragment();
+    }
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        layout = inflater.inflate(R.layout.fragment_main_music, container, false);
+        return layout;
+    }
+}
